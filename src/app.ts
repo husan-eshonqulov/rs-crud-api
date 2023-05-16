3770fc474576c185cc42b81a2c0c8ee7de8c7dc8
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-05-16 18:08:17 +0500

    add NODE_ENV

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Express } from 'express';
+import dotenv from 'dotenv';
 
 import usersRoute from './routes/users';
 import { notFound } from './controllers/not-found';
+
+dotenv.config();
 
 const app: Express = express();
 
@@ -15,5 +18,7 @@ app.use(notFound);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`server is listening on port ${port}...`);
+  console.log(
+    `server is listening in ${process.env.NODE_ENV} mode on port ${port}...`
+  );
 });
